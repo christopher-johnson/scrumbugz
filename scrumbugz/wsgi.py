e6fc9b5817fecc08890469d2587adbdcc001cecc
Christopher Johnson (WMDE) <chjohnson@wmde.pmtpa.wmflabs>
2014-01-15 13:20:42 +0000

    update settings-dist.py and INSTALL

--- scrumbugz/wsgi.py
+++ scrumbugz/wsgi.py
@@ -14,8 +14,25 @@ framework.
 
 """
 import os
+import sys
+import site
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrumbugz.settings")
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/usr/local/django/scrumbugz/venv/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/usr/local/django')
+sys.path.append('/usr/local/django/scrumbugz')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/usr/local/django/scrumbugz/venv/bin/activate_this.py")
+execfile('/usr/local/django/scrumbugz/venv/bin/activate_this.py', dict(__file__='/usr/local/django/scrumbugz/venv/bin/activate_this.py'))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
